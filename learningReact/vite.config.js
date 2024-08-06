2f525fd731e060621daee95766f68034444fc5b9
Tony <antlouthan@gmail.com>
2024-08-06 12:00:51 -0400

    one more time

--- learningReact/vite.config.js
+++ learningReact/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: "/intro_to_react",
+    base: "/learningReact",
     plugins: [react()],
 })

623706a7f5ad8ef63298247572420d782d61249e
Tony <antlouthan@gmail.com>
2024-08-06 12:04:43 -0400

    okay trying a diff route

--- learningReact/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: "/intro_to_react/learningReact",
-    plugins: [react()],
-})
